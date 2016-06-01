828ddcea379bf4b1921bb1409ae703f9c84f0900
pedroccrl <pedrocamara@MacBook-Pro-de-Pedro-2.local>
2016-06-01 13:37:49 -0300

    Teste vscode

--- Web/Scripts/apisocrata.js
+++ Web/Scripts/apisocrata.js
@@ -6,8 +6,7 @@ function progresso(evt) {
     document.getElementById("content").innerHTML = evt.loaded;
 }
 
-function loadData()
-{
+function loadData() {
     var xhttp = new XMLHttpRequest();
     xhttp.onprogress = progresso;
     xhttp.onreadystatechange = function ()
@@ -15,13 +14,13 @@ function loadData()
         if (xhttp.readyState == 4 && xhttp.status == 200)
         {
             dados = JSON.parse(xhttp.responseText);
-            for (var i = 0; i < dados.length; i++) {
+            for (var i = 0; i < dados.length; i++) 
+            {
                 document.getElementById("content").innerHTML += "<br>Nome da Área: ";
                 document.getElementById("content").innerHTML += dados[i].community_area_name;
                 document.getElementById("content").innerHTML += "<br> Renda per capita: R$";
                 document.getElementById("content").innerHTML += dados[i].per_capita_income_;
             }
-            
         }
     };
     xhttp.open("GET", "https://data.cityofchicago.org/resource/jcxq-k9xf.json?$select=*", true);
