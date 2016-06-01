21e58472e165a84d1f7ecb8cb5294d1c131dd901
pedroccrl <pedrocamara@MacBook-Pro-de-Pedro-2.local>
2016-06-01 13:58:16 -0300

    teste 2

--- Web/Scripts/apisocrata.js
+++ Web/Scripts/apisocrata.js
@@ -6,7 +6,7 @@ function progresso(evt) {
     document.getElementById("content").innerHTML = evt.loaded;
 }
 
-function loadData() {
+function loadDataInDiv(divId) {
     var xhttp = new XMLHttpRequest();
     xhttp.onprogress = progresso;
     xhttp.onreadystatechange = function ()
@@ -16,15 +16,15 @@ function loadData() {
             dados = JSON.parse(xhttp.responseText);
             for (var i = 0; i < dados.length; i++) 
             {
-                document.getElementById("content").innerHTML += "<br>Nome da Área: ";
-                document.getElementById("content").innerHTML += dados[i].community_area_name;
-                document.getElementById("content").innerHTML += "<br> Renda per capita: R$";
-                document.getElementById("content").innerHTML += dados[i].per_capita_income_;
+                document.getElementById(divId).innerHTML += "<br><br>Nome da Área: ";
+                document.getElementById(divId).innerHTML += dados[i].community_area_name;
+                document.getElementById(divId).innerHTML += "<br> Renda per capita: $";
+                document.getElementById(divId).innerHTML += dados[i].per_capita_income_;
             }
         }
     };
     xhttp.open("GET", "https://data.cityofchicago.org/resource/jcxq-k9xf.json?$select=*", true);
     xhttp.send();
     totalBytes = xhttp.getResponseHeader("Content-Length");
-    document.getElementById("content").innerHTML = totalBytes;
+    document.getElementById(divId).innerHTML = totalBytes;
 }
