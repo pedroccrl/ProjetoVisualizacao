f1e14809437951897172dbb698eab0ce6953d8e4
Pedro Câmara <pedroccrl@gmail.com>
2016-12-12 20:09:41 -0200

    Ultimo

---
+++ Novo ASP MVC/Novo ASP MVC/Controllers/ContaController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Novo_ASP_MVC.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Conta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Conta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conta/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Conta/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conta/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Conta/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
