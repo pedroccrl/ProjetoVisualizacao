3e2bf541fdb90523989f9be7c5e1309489e85022
Pedro Câmara <pedroccrl@gmail.com>
2017-04-19 02:41:19 -0300

    CKAN Crawler

---
+++ Novo ASP MVC/Novo ASP MVC/Controllers/Default1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Novo_ASP_MVC.Controllers
+{
+    public class Default1Controller : ApiController
+    {
+        // GET: api/Default1
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default1/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default1
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Default1/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default1/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
